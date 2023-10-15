050b4276200b5331cf7a280534564da3017a4e5a
Ljf227 <ljfrotton@gmail.com>
2023-10-15 17:43:26 -0400

    frontend structure starting now

---
+++ client/the-manager/app/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <section>
+        <header> 
+            <h1></h1>
+        </header>
+      </section>
+    </main>
+  )
+}
