7698393e9b050a82b72659d56eaecd96238b6349
Ljf227 <ljfrotton@gmail.com>
2023-10-17 18:00:27 -0400

    added kind os a sign out button

--- nextapp/the-manager/app/profile/page.js
+++ nextapp/the-manager/app/profile/page.js
@@ -1,6 +1,8 @@
 import { getServerSession } from "next-auth";
 import { options } from "../api/auth/[...nextauth]/options";
+import { redirect } from "next/navigation";
 import { Prof } from "@/components/Profile";
+import { SignOut } from "@/components/SignOut";
 
 
 export default async function Profile(){
@@ -10,15 +12,14 @@ export default async function Profile(){
 
     if(!session.user.name)
     {
-        alert("you do not have a profile you should not be here yet"); 
-        return(
-            <section>
-                nice try
-            </section>)
+        redirect('/'); 
     }else{
 
         return(
             <main className="min-h-screen bg-gradient-to-t from-slate-200 to-slate-400">
+                <section>   
+                    <SignOut/>
+                </section>
                 <section>
                     <header className="text-center text-7xl font-extrabold">
                         <h1>Profile</h1>
--- nextapp/the-manager/components/SignOut.js
+++ nextapp/the-manager/components/SignOut.js
@@ -2,3 +2,13 @@
 
 import { signOut } from 'next-auth/react'; 
 
+export const SignOut = () =>{
+    return(
+       <button className="bg-slate-900 px-5 py-2 text-white"
+       onClick={() => signOut()}
+       type="button">
+        Sign Out
+       </button> 
+    )
+}
+
